394512df2cc901198fb4515c6c14a3e25b96907e
huajianjiu <lastranger8811@gmail.com>
2018-03-05 23:48:12 +0900

    py3 compatibile

--- data_iter.py
+++ data_iter.py
@@ -63,7 +63,7 @@ class DisDataIter(object):
         self.data = real_data_lis + fake_data_lis
         self.labels = [1 for _ in range(len(real_data_lis))] +\
                         [0 for _ in range(len(fake_data_lis))]
-        self.pairs = zip(self.data, self.labels)
+        self.pairs = list(zip(self.data, self.labels))
         self.data_num = len(self.pairs)
         self.indices = range(self.data_num)
         self.num_batches = int(math.ceil(float(self.data_num)/self.batch_size))
